c67be0f631ef4bd64c3f629d78f056fb22949a8f
Bachpx <bachpx195@gmail.com>
2018-03-15 10:31:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const rect = require('./Shape/rectangle');
+
+console.log(`The circumference of the rectangle width 10, height 20 is ${rect.circumference(10, 20)}`);
+
+console.log("hello world");
+
+console.log(`${rect.currentDateTime}`)
+console.log(`${rect.directoryName}`)
+console.log(`${rect.fileName}`)
